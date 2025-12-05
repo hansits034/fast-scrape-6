a59aea402534c0b55474cd01f78b62a7896cf00e
hansits034 <162028121+hansits034@users.noreply.github.com>
2025-12-05 14:13:39 +0700

    Update scrape2.py

--- scrape2.py
+++ scrape2.py
@@ -14,7 +14,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 # Batch 1: 0 sampai 4000
 # Batch 2: 4000 sampai 8000
 # Batch 3: 8000 sampai 12000, dst...
-BATCH_START = 50     
+BATCH_START = 3000     
 BATCH_END   = 5000  
 
 CSV_FILE = "hoax_data_complete.csv"
